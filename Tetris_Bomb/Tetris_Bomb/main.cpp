16058c883a32f81dd9e3eafb76ce0c995e166a1b
Donghyun Kim <newdhkim0615@gmail.com>
2025-12-04 16:24:34 +0900

    [Fix] 게임 종료 수정

--- Tetris_Bomb/Tetris_Bomb/main.cpp
+++ Tetris_Bomb/Tetris_Bomb/main.cpp
@@ -1,6 +1,11 @@
 #include "Game.h"
 
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
 int main() {
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     system("mode con: cols=120 lines=40");
     Tetris::Game game;
     game.run();
